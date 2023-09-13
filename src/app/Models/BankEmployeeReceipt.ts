11c9b4cc3f0b14800d075ed73360c9d38f946740
mark <jack.jones@techbynexarise.com>
2023-09-14 02:20:38 +0500

    update

--- src/app/Models/BankEmployeeReceipt.ts
+++ src/app/Models/BankEmployeeReceipt.ts
@@ -4,7 +4,7 @@ import { PaymentModel } from "./PaymentModel";
 export class BankEmployeeReceiptDto  {
   id: number;
   tenantId: number;
-  memberId: number;
+  bankEmployeeId: number;
   bankEmployee: BankEmployeeDto;
   payments: PaymentModel[] | undefined;
   lookUpId: number;
